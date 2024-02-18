56e7be1c00c46f366f491f296642ea5be98fb627
Shaiban Luqman <shabzie3@gmail.com>
2024-02-18 18:16:32 +0000

    edit front-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,38 @@
 import { Button, Card, CardBody, CardHeader, Image } from "@nextui-org/react";
 import NextImage from "next/image";
-import { NewNoteIcon } from "./components/icons/NewNoteIcon";
+import PencilSquareIcon from "./components/icons/PencilSquareIcon";
 
 export default async function App() {
   return (
     <>
-      <div className="grid gap-y-14 mt-20 mx-14 place-content-center">
-        <div>
-          <h1 className="text-4xl md:text-6xl lg:text-7xl font-medium tracking-normal leading-snug">
-            One place for all your impromptu notes.
-          </h1>
-        </div>
-        <div>
-          <Image
-            // className="mx-40"
-            as={NextImage}
-            width={600}
-            height={600}
-            alt="NextUI hero Image"
-            src="https://eoimages.gsfc.nasa.gov/images/imagerecords/99000/99676/Alps.A2002273.1015_th.jpg"
-          />
-        </div>
-      </div>
-      <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
-        <Card className="col-span-12 sm:col-span-4 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <h4 className="text-white font-medium text-large">
-              Stream the Acme event
-            </h4>
-          </CardHeader>
-          <CardBody className="place-content-center">
-            <div className="place-self-center">
-              <Button isIconOnly>
-                <NewNoteIcon />
-              </Button>
+      <div className="grid gap-y-14 mt-20 mx-10">
+        <h1 className="text-center text-4xl md:text-6xl lg:text-7xl font-medium tracking-normal leading-snug">
+          One place for all your impromptu notes.
+        </h1>
+        <h3 className="text-center text-lg md:text-3xl lg:text-4xl font-medium tracking-normal leading-snug">
+          Use yanta notes to quickly jot anything down, and access them from
+          anywhere.
+        </h3>
+        <div className="flex justify-center">
+          <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-zinc-800 dark:border-zinc-700">
+            <div className="p-5">
+              <div className="mb-2 flex justify-center ">
+                <PencilSquareIcon />
+              </div>
+              <h5 className="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                Have last minute thoughts on that project? Jot it down quick here.
+              </h5>
+              <div className="flex justify-center">
+                <Button
+                  href="/notes/new"
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-main-color rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-gray-800"
+                >
+                  Write note
+                </Button>
+              </div>
             </div>
-          </CardBody>
-        </Card>
-        <Card className="col-span-12 sm:col-span-4 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              Plant a tree
-            </p>
-            <h4 className="text-white font-medium text-large">
-              Contribute to the planet
-            </h4>
-          </CardHeader>
-        </Card>
-        <Card className="col-span-12 sm:col-span-4 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              Supercharged
-            </p>
-            <h4 className="text-white font-medium text-large">
-              Creates beauty like a beast
-            </h4>
-          </CardHeader>
-        </Card>
+          </div>
+        </div>
       </div>
     </>
   );
