cfe0c83dc9e9b1e6421f298985ede97ea2f0b1b7
Shaiban Luqman <shabzie3@gmail.com>
2024-03-02 19:49:59 +0000

    speed insights

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,16 +4,16 @@ import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 import { dark } from "@clerk/themes";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   return (
     <NextUIProvider navigate={router.push}>
       <NextThemesProvider attribute="class" defaultTheme="dark">
-        <ClerkProvider
-          
-        >
+        <ClerkProvider>
           {children}
+          <SpeedInsights />
         </ClerkProvider>
       </NextThemesProvider>
     </NextUIProvider>
