c55bae99c18715d99652259dadbd0ab9ef9981f8
Shaiban Luqman <shabzie3@gmail.com>
2024-03-03 12:06:28 +0000

    console logging to middleware

--- middleware.ts
+++ middleware.ts
@@ -10,14 +10,17 @@ export default authMiddleware({
   afterAuth(auth, req, evt) {
     // If the user is logged in and trying to access a protected route, allow them to access route
     if (auth.userId && !auth.isPublicRoute) {
+      console.log("user is logged in")
       return NextResponse.next();
     }
     // Handle users who aren't authenticated
     if (!auth.userId && !auth.isPublicRoute) {
+      console.log("user is not authenticated, redirecting to login")
       return redirectToSignIn({ returnBackUrl: req.url });
     }
 
     // Allow users visiting public routes to access them
+    console.log("going to a public route")
     return NextResponse.next();
   },
 });
